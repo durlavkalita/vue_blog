1ecf94a0561d74e4e6b2476cfc09536699e3d48b
Durlav <durlavk98@gmail.com>
2022-08-01 20:10:04 +0530

    vite scaffolding + initial routes

---
+++ README.md
@@ -0,0 +1,21 @@
+# Vue 3 + Vite
+
+This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+
+## Recommended IDE Setup
+
+- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
+
+- routes-
+
+/posts -> list of all posts
+/posts/:id -> individual post
+/posts/:id/update -> update a post give access to author only
+/posts/:id/delete -> delete a post
+
+/authors -> list of all authors
+/author/:id -> individual author details
+/author/:id/create -> create post by author with :id
+
+- install vue router - npm install vue-router@4
+
---
+++ src/router.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+import PostsList from './pages/posts/PostsList.vue'
+import PostDetail from './pages/posts/PostDetail.vue'
+import PostCreate from './pages/posts/PostCreate.vue'
+import PostUpdate from './pages/posts/PostUpdate.vue'
+import AuthorsList from './pages/authors/AuthorsList.vue'
+import AuthorDetail from './pages/authors/AuthorDetail.vue'
+import AuthorRegister from './pages/authors/AuthorRegister.vue'
+import NotFound from './pages/NotFound.vue'
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        { path: '/', redirect: '/posts' },
+        { path: '/posts', component: PostsList },
+        {
+            path: '/posts/:id',
+            component: PostDetail,
+            props: true,
+            children: [
+                { path: 'update', component: PostUpdate },
+            ]
+        },
+        { path: '/authors', component: AuthorsList },
+        { path: '/authors/register', component: AuthorRegister },
+        {
+            path: '/authors/:id',
+            component: AuthorDetail,
+            props: true,
+            children: [
+                { path: 'create', component: PostCreate },
+            ]
+        },
+        { path: '/:notFound(.*)', component: NotFound },
+    ],
+});
+
+export default router;
